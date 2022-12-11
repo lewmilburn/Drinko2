c2e6e444d41924eaf497d17c82d0bb70c9fda27f
lewmilburn <63267144+lewmilburn@users.noreply.github.com>
2022-12-11 21:51:31 +0000

    Testing file reading

--- assets/js/gameManager.js
+++ assets/js/gameManager.js
@@ -2,15 +2,13 @@ let Truths = null;
 let Dares = null;
 
 function LoadGame() {
-    let LoadedTruths = false;
-    let LoadedDares = false;
-
     fetch("https://drinko.co.uk/data/truths.json")
         .then(response => response.json())
         .then(json => Truths = json)
-    /*fetch("./data/dares.json")
+    fetch("https://drinko.co.uk/data/dares.json")
         .then(response => response.json())
-        .then(json => Dares = json);*/
+        .then(json => Dares = json);
+    
     while (true) {
         if (Truths !== null && Dares !== null) {
             ShowScreen(2,3);
