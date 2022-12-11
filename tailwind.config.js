d15f2a06e626e92d8fa64492b781ab48e79b7714
lewmilburn <63267144+lewmilburn@users.noreply.github.com>
2022-12-11 19:43:09 +0000

    Update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./**/*.{html,js}", "./*.{html,js}", "./index.html"],
-    theme: {
-        extend: {},
-    },
-    plugins: [],
+  content: [
+    './**/*.html'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 }
