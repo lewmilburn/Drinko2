a7757f0a841cbc9cc97067c96556ddb196ee2166
lewmilburn <63267144+lewmilburn@users.noreply.github.com>
2022-12-12 14:59:33 +0000

    Updated PWA code

--- assets/js/appManager.js
+++ assets/js/appManager.js
@@ -3,19 +3,20 @@ if ('serviceWorker' in navigator) {
 }
 
 
-// Initialize deferredPrompt for use later to show browser install prompt.
 let deferredPrompt;
-
 window.addEventListener('beforeinstallprompt', (e) => {
-    Log('beforeinstallprompt','event listener started',3);
-    // Prevent the mini-infobar from appearing on mobile
-    e.preventDefault();
-    // Stash the event so it can be triggered later.
     deferredPrompt = e;
-    // Update UI notify the user they can install the PWA
-    showInstallPromotion();
-    // Optionally, send analytics event that PWA install promo was shown.
-    Log('beforeinstallprompt','event listener ended',3);
+});
+
+const installApp = document.getElementById('InstallPWA');
+installApp.addEventListener('click', async () => {
+    if (deferredPrompt !== null) {
+        deferredPrompt.prompt();
+        const { outcome } = await deferredPrompt.userChoice;
+        if (outcome === 'accepted') {
+            deferredPrompt = null;
+        }
+    }
 });
 
 function showInstallPromotion() {
