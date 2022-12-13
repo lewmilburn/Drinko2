4034ff6eaa9d7c64c9bb7fdfaf16551b9d01979e
lewmilburn <63267144+lewmilburn@users.noreply.github.com>
2022-12-13 02:14:29 +0000

    Changed round selection

--- assets/js/screenManager.js
+++ assets/js/screenManager.js
@@ -107,14 +107,11 @@ function DisplayScreen(number) {
                 <button class="btn-sm btn-blue" onClick="AddPlayer();">Add</button>
             </div>
             <ul id="HomePlayerList"></ul>
-            <br>
             <select id="HomeSelectRounds" class="input">
-                <option value="-1" selected>Max Rounds</option>
-                <option value="-1" selected>No Limit</option>
+                <option value="-1" selected>All the Rounds</option>
                 <option value="10">10 Rounds</option>
-                <option value="10">25 Rounds</option>
-                <option value="10">50 Rounds</option>
             </select>
+            <br>
             <div class="snap-x snap-mandatory">
                 <button class="btn-mode-select btn-green snap-center" onClick="LoadGame('Lightweight');">
                     Lightweight
