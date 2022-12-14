9ff0f5ab5fce9c5a973c45391b5e791fbf205685
lewmilburn <63267144+lewmilburn@users.noreply.github.com>
2022-12-14 19:59:25 +0000

    Changed default round count.

--- assets/js/screenManager.js
+++ assets/js/screenManager.js
@@ -124,7 +124,7 @@ function DisplayScreen(number) {
             <ul id="HomePlayerList"></ul>
             <select id="HomeSelectRounds" class="input">
                 <option value="50">50 Rounds</option>
-        <option value="25" selected>25 Rounds</option>
+                <option value="25" selected>25 Rounds</option>
                 <option value="15">15 Rounds</option>
                 <option value="10">10 Rounds</option>
                 <option value="10">5 Rounds</option>
