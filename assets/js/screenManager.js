1cb80c1ab3007cd1677d7d1591701342f13fe2ac
lewmilburn <63267144+lewmilburn@users.noreply.github.com>
2022-12-14 00:37:40 +0000

    Updating homescreen buttons.

--- assets/js/screenManager.js
+++ assets/js/screenManager.js
@@ -130,7 +130,7 @@ function DisplayScreen(number) {
                 <option value="10">5 Rounds</option>
             </select>
             <br><br>
-            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2">
+            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2">
                 <button class="btn-mode-select btn-green snap-center" onClick="LoadGame('Lightweight');">
                     Lightweight
                 </button>
