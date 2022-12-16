76407f10ff9c1a3c3b6b904a54cbcf2a7451945e
lewmilburn <63267144+lewmilburn@users.noreply.github.com>
2022-12-16 13:59:04 +0000

    Minified JavaScript

--- assets/js/src/deviceManager.js
+++ assets/js/src/deviceManager.js
@@ -1,6 +1,3 @@
 if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
     let nav = document.getElementById('nav');
-
-    nav.classList.add('hidden');
-    nav.classList.remove('flex');
 }
