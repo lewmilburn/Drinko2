c2cdbf331a9d28c5c75afeb6a233f7d83111d561
lewmilburn <63267144+lewmilburn@users.noreply.github.com>
2023-02-17 16:14:18 +0000

    Attempted fix on random undefined players

--- assets/js/src/playerManager.js
+++ assets/js/src/playerManager.js
@@ -16,7 +16,7 @@ function AddPlayer() {
     if (Object.keys(Players).length <= 15) {
         if (Player === '69') { alert('Nice ;)'); }
         else if (Player === '420') { alert('Blaze it!'); }
-        else if (Player === '') { return; }
+        else if (Player === '' || Player === undefined) { return; }
 
         if (List.innerHTML === null) {
             List.innerHTML = '<li>' + Player + '</li>';
@@ -43,10 +43,13 @@ function GetRandomPlayer() {
 
     let PlayerID = Math.floor(Math.random() * (Object.keys(Players).length));
 
-    if (Math.floor(Math.random() * 2) == 0) {
-        NextPlayer = GetUserLowestTurns();
-    } else {
-        NextPlayer = Object.keys(Players)[PlayerID];
+    NextPlayer = undefined;
+    while (NextPlayer !== undefined) {
+        if (Math.floor(Math.random() * 2) == 0) {
+            NextPlayer = GetUserLowestTurns();
+        } else {
+            NextPlayer = Object.keys(Players)[PlayerID];
+        }
     }
 
     Log('GetRandomPlayer','Selected: ' + NextPlayer,3);
@@ -56,9 +59,12 @@ function GetRandomPlayer() {
 function GetUserLowestTurns() {
     Log('GetUserLowestTurns','started',4);
 
-    let LowestUser = Object.keys(Turns).find(key => Turns[key] === (Object.keys(Turns).reduce((acc, val) => {
-        return Math.min(acc, Turns[val]);
-        }, Infinity)));
+    let LowestUser = undefined;
+    while (LowestUser !== undefined) {
+        let LowestUser = Object.keys(Turns).find(key => Turns[key] === (Object.keys(Turns).reduce((acc, val) => {
+            return Math.min(acc, Turns[val]);
+            }, Infinity)));
+    }
 
     Log('GetUserLowestTurns','Selected ' + LowestUser,3);
 
@@ -78,11 +84,13 @@ function GetRandomReplacePlayer() {
             NextPlayer = Object.keys(Players)[PlayerID];
         }
 
-        if (NextSecondPlayer !== NextPlayer) {
-            Log('GetRandomReplacePlayer','Break RP: '+NextSecondPlayer+' - NP: '+NextPlayer,3);
-            break;
-        } else {
-            Log('GetRandomReplacePlayer','No break RP: '+NextSecondPlayer+' - NP: '+NextPlayer,3);
+        if (NextSecondPlayer === undefined || NextPlayer === undefined) {
+            if (NextSecondPlayer !== NextPlayer) {
+                Log('GetRandomReplacePlayer','Break RP: '+NextSecondPlayer+' - NP: '+NextPlayer,3);
+                break;
+            } else {
+                Log('GetRandomReplacePlayer','No break RP: '+NextSecondPlayer+' - NP: '+NextPlayer,3);
+            }
         }
     }
 
